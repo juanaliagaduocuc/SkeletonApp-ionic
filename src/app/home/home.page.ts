bb4ce8c04676746a9d96e0e193062e885f2f8896
juanaliagaduocuc <jua.aliaga@duocuc.cl>
2026-05-30 16:14:37 -0400

    dump 2
    diseño modelo mockup, validaciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
-import { RouterLink } from '@angular/router';
+import {  IonHeader, 
+          IonToolbar, 
+          IonTitle, 
+          IonContent, 
+          IonButton,
+          IonCard,
+          IonCardContent,
+          IonCardHeader,
+          IonCardTitle, 
+          IonFooter} from '@ionic/angular/standalone';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
+  imports: [  IonHeader, 
+              IonToolbar, 
+              IonTitle, 
+              IonContent, 
+              IonButton, 
+              RouterLink,
+              IonCard,
+              IonCardContent,
+              IonCardHeader,
+              IonCardTitle, 
+              IonFooter],
 })
 export class HomePage {
-  constructor() {}
+  data: any;
+  constructor(  
+    private activeroute: ActivatedRoute,
+    private router: Router) {
+      this.activeroute.queryParams.subscribe ( params => {
+        if (this.router.currentNavigation()?.extras?.state){
+          this.data = this.router.currentNavigation()?.extras?.state?.['usuario'];
+        }
+      });
+  }
+  
+  cerrar(){
+    this.data = "";
+  }
+
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonInput } from '@ionic/angular/standalone';
-
+import {  IonContent, 
+          IonHeader, 
+          IonTitle, 
+          IonToolbar, 
+          IonBackButton, 
+          IonButtons, 
+          IonInput,
+          IonCard,
+          IonCardContent,
+          IonCardHeader,
+          IonCardTitle, 
+          IonFooter, 
+          IonButton, 
+          IonText
+          } from '@ionic/angular/standalone';
+import { NavigationExtras, Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonButtons, IonInput]
+  imports:  [IonContent,
+            IonHeader,
+            IonTitle,
+            IonToolbar, 
+            CommonModule, 
+            FormsModule, 
+            IonBackButton, 
+            IonButtons, 
+            IonInput,
+            IonCard,
+            IonCardContent,
+            IonCardHeader,
+            IonCardTitle, 
+            IonFooter, 
+            IonButton, 
+            IonText]
 })
 export class LoginPage implements OnInit {
+  
+  usuario={
+    user:'',
+    pass:''
+  }
 
-  constructor() { }
+  constructor(private router: Router) { 
+    
+  }
 
   ngOnInit() {
   }
+  
+  ingresar(){
+    let navigationExtras: NavigationExtras = {
+      state: {
+        usuario: this.usuario
+      }
+    };
+    this.router.navigate(['/home'],navigationExtras);
+  }
+  
 
 }
