5aafc091b4d165f1efc5324082f0c744bb513e1a
juanaliagaduocuc <jua.aliaga@duocuc.cl>
2026-05-31 15:03:12 -0400

    integracion botones angular mat
    cambio de la mayoria de inputs a angular material

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,27 +8,63 @@ import {  IonHeader,
           IonCardContent,
           IonCardHeader,
           IonCardTitle, 
+          IonCardSubtitle, 
           IonFooter} from '@ionic/angular/standalone';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
+interface Nvl{
+  nivel: string,
+  desc: string
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  providers: [provideNativeDateAdapter()],
   imports: [  IonHeader, 
               IonToolbar, 
               IonTitle, 
               IonContent, 
-              IonButton, 
+              IonButton,
               RouterLink,
               IonCard,
               IonCardContent,
               IonCardHeader,
               IonCardTitle, 
-              IonFooter],
+              IonCardSubtitle, 
+              IonFooter, 
+              MatFormFieldModule, 
+              MatInputModule, 
+              MatSelectModule, 
+              MatDatepickerModule, 
+              MatButtonModule, 
+              FormsModule],
 })
 export class HomePage {
+  usuario={
+    nombre:'',
+    apellido:'',
+    educacion:'',
+    fec_nac:''
+  }
   data: any;
+  nvls: Nvl[] = [
+    {nivel: '0', desc: 'Basica Incompleta'},
+    {nivel: '1', desc: 'Basica Completa'},
+    {nivel: '2', desc: 'Media Incompleta'},
+    {nivel: '3', desc: 'Media Completa'},
+    {nivel: '4', desc: 'Superior Incompleta'},
+    {nivel: '5', desc: 'Superior Completa'},
+    {nivel: '6', desc: 'Tecnico Incompleta'},
+    {nivel: '7', desc: 'Tecnico Completa'},
+  ];
   constructor(  
     private activeroute: ActivatedRoute,
     private router: Router) {
@@ -42,5 +78,13 @@ export class HomePage {
   cerrar(){
     this.data = "";
   }
-
+  limpiar(){
+    this.usuario.nombre = '';
+    this.usuario.apellido = '';
+    this.usuario.educacion = '';
+    this.usuario.fec_nac = '';
+  }
+  mostrar(){
+    
+  }
 }
